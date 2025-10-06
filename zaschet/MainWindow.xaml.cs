af20e289fdb65798dd61bee5e223d0327966ff2e
Solovev.A.D <Solovev.A.D@PC205WIN-07>
2025-10-06 14:24:59 +0300

    Add project files.

---
+++ zaschet/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace StudentAttendanceSystem
+{
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string login = LoginTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (login == "admin" && password == "admin")
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
